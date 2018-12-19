d46c4aad3613562baeb5fe11c4b8fa4b7d99f973
Cassio <cassiowt@gmail.com>
2018-12-19 09:25:05 -0200

    Triangulo

--- src/test/java/exemplo/TrianguloTest.java
+++ src/test/java/exemplo/TrianguloTest.java
@@ -2,9 +2,8 @@ package exemplo;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.EnumSource;
-
-import java.util.concurrent.TimeUnit;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -16,7 +15,7 @@ public class TrianguloTest {
 
     @BeforeEach
     public void init() {
-        triangulo = new Triangulo(2,2,3);
+    //    triangulo = new Triangulo(2,2,3);
     }
 
     @ParameterizedTest
@@ -25,6 +24,15 @@ public class TrianguloTest {
         assertNotNull(tipoTriangulo);
     }
 
+    @ParameterizedTest (name = "Lado A = {0}, Lado B = {1}, Lado C = {2} :: {3}")
+    @CsvSource({"1,2,2, INEXISTENTE",
+               "1,1,2, ESCALENO",
+               "2,2,2, EQUILATERO "})
+    public void validaTriangulo(int um, int dois, int tres, TipoTriangulo tipoTriangulo){
+        triangulo = new Triangulo(um,dois,tres);
+        System.out.println(triangulo.validaTriangulo());
+        assertTrue( triangulo.validaTriangulo().equals(tipoTriangulo));
+    }
 
 
 }
